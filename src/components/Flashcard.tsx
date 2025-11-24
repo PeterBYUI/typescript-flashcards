27aa681223764c2346beb25d7f4a8af40e8d2943
PeterBYUI <pierre.gavino@gmail.com>
2025-11-24 11:32:50 +0100

    feat(ui): add full UI for creating new flashcards (no backend logic yet)

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -6,9 +6,10 @@ import Button from "./Button";
 
 type FlashcardComponentProps = {
     flashcard?: FlashcardModel;
+    isNew: boolean
 }
 
-export default function Flashcard({ flashcard }: FlashcardComponentProps) {
+export default function Flashcard({ flashcard, isNew }: FlashcardComponentProps) {
 
     const [card, setCard] = useState<FlashcardModel | undefined>(flashcard);
 
@@ -40,9 +41,13 @@ export default function Flashcard({ flashcard }: FlashcardComponentProps) {
             <label className="text-[rgba(100,190,171)] font-semibold">Answer</label>
             <Input styling="w-1/1" value={card?.answer || ""} onChange={(e) => handleTextChange(e, false)} />
         </div>
-        <Button styling="translate-y-[20%] hover:text-red-400"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-        </svg>
+        <Button styling={!isNew ? "translate-y-[20%] hover:text-red-400" : "translate-y-[20%] hover:text-[rgba(100,190,171)]"}>
+            {!isNew ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+            </svg>
+            }
         </Button>
     </div>
 }
--- src/pages/Flashcards.tsx
+++ src/pages/Flashcards.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../store/UserContext";
 import { fetchFlashcard } from "../utils/http";
 
@@ -23,23 +23,39 @@ export default function Flashcards() {
         enabled: !!user?.id
     });
 
+    const [isAdding, setIsAdding] = useState<boolean>(false);
+
+    function handleToggleAddition() {
+        setIsAdding((previousValue) => !previousValue)
+    }
+
     return <>
         <section className="py-16 px-4 sm:px-8 xl:px-16">
             <Card styling="p-5 md:w-3/3 lg:w-3/4 xl:w-2/3">
                 <Title title="Add flashcards" />
                 {!flashcards && <p>No flashcards.</p>}
                 {isPending && <Spinner />}
-                {flashcards && <div className="flex flex-col gap-4">
-                    {flashcards.map((flashcard) => <Flashcard flashcard={flashcard} />)}
-                </div>}
+                <div className="flex flex-col gap-4">
+                    {flashcards &&
+                        flashcards.map((flashcard) => <Flashcard flashcard={flashcard} isNew={false} />)
+                    }
+                    {isAdding && <div>
+                        <Flashcard flashcard={{ id: "", userId: "", question: "", answer: "" }} isNew={true} />
+                    </div>}
+                </div>
                 {isError && <Error errors={["Something went wrong. Please try again later."]} />}
+
                 <div className="mt-8">
-                    <Button styling="text-[rgba(100,190,171)] hover:text-[rgb(79,151,136)]">
+                    <Button onClick={handleToggleAddition} styling={!isAdding ? "text-[rgba(100,190,171)] hover:text-[rgb(79,151,136)]" : "text-red-400 hover:text-red-500"}>
                         <div className="flex gap-2 items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8">
-                                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
+                            {isAdding ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8">
+                                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clipRule="evenodd" />
                             </svg>
-                            <p>New flashcard</p>
+                                : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8">
+                                    <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
+                                </svg>}
+
+                            <p>{isAdding ? "Cancel" : "New flashcard"}</p>
                         </div>
                     </Button>
                 </div>
